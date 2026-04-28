7a2e4f1e20a25b2c88d4c336f2fec47a1ec353ba
mskgmalay09-cpu <ms.kgmalay09@gmail.com>
2026-04-28 20:24:16 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const app = express();
 app.use(express.json());
 
-const PAGE_ACCESS_TOKEN ='EAANw5phuolEBRZATSinvklp0yhMZBOmD13zoGmB2pW67llYx0rMSXVSm74wGh3APa5vfyHGCDuhzD429AIO8ZAwZAmAtemHdBP6D8wFvgRa8w2eSZC9gKirj5gpj7wCoPeXYpisf1wUmlNSS7ybZAzVGZC8vFtz82snA6Ch0MBudYYGHSSJdPxY5WybJnoepf8p257QhMMufGSmBg78S2GtPCXL'
+const PAGE_ACCESS_TOKEN = 'EAANw5phuolEBRZATSinvklp0yhMZBOmD13zoGmB2pW67llYx0rMSXVSm74wGh3APa5vfyHGCDuhzD429AIO8ZAwZAmAtemHdBP6D8wFvgRa8w2eSZC9gKirj5gpj7wCoPeXYpisf1wUmlNSS7ybZAzVGZC8vFtz82snA6Ch0MBudYYGHSSJdPxY5WybJnoepf8p257QhMMufGSmBg78S2GtPCXL'
 app.get('/webhook', (req, res) => {
   if (req.query['hub.mode'] === 'subscribe' &&
       req.query['hub.verify_token'] === VERIFY_TOKEN) {
